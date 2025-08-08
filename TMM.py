fix(TMM): fix error in transmission spectrum calculation

TMM.py
@@ -160,7 +160,7 @@ def TMM_solver(thicknesses, refractive_indices, n_bot, n_top, k, theta, pol = 'T
         # reflection 
         Reflection = torch.pow(torch.abs(S_stack[:,:,:,:,0,1]), 2) / torch.pow(torch.abs(S_stack[:,:,:,:,0,0]), 2)
         Reflection = Reflection.double()
-        Transmission = torch.pow(torch.det(S_stack), 2) / torch.pow(torch.abs(S_stack[:,:,:,:,0,0]), 2)
+        Transmission = torch.pow(torch.det(torch.abs(S_stack)), 2) / torch.pow(torch.abs(S_stack[:,:,:,:,0,0]), 2)
         Transmission = Transmission.double()
     else:
         if pol in ['TM', 'TE']: